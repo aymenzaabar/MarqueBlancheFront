c47703d8031a7122924b1d1f152d6d6756061495
Aymen ZAABAR <aymen.zaabar@esprit.tn>
2023-10-13 19:58:36 +0200

    usersmanagement

--- src/app/Modules/usersmanagement/usersmanagement/usersmanagement.component.ts
+++ src/app/Modules/usersmanagement/usersmanagement/usersmanagement.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { UserService } from 'src/app/services/userservice/user.service';
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-usersmanagement',
@@ -8,9 +13,63 @@ import { UserService } from 'src/app/services/userservice/user.service';
 })
 export class UsersmanagementComponent implements OnInit {
   users: any[] = [];
+  dismissible = true;
+ 
+  user = {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    username: '',
+    password: '',
+    role: 'Collaborateur', // Default role
+  };
+  submitted = false;
 
-  constructor(private userService: UserService) { }
 
+  formModal: BsModalRef;
+  form = {
+    keyboard: true,
+    class: "modal-dialog-centered modal-sm"
+  };
+
+  addUserForm: FormGroup;
+
+
+ 
+
+  constructor(private userService: UserService,private http: HttpClient, private formBuilder: FormBuilder,
+    private modalService: BsModalService) {
+      this.addUserForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', Validators.email],
+        phoneNumber: [''],
+        username: [''],
+        password: [''],
+        role: ['Collaborateur'], // You can set a default value here
+      });
+     }
+
+
+
+    openFormModal(modalForm: TemplateRef<any>) {
+      this.formModal = this.modalService.show(modalForm, this.form);
+    }
+    registerUser() {
+      // Send a POST request to the API endpoint
+      this.http
+        .post('http://localhost:8020/api/auth/signup', this.user)
+        .subscribe(
+          (response) => {
+            console.log('User registered successfully', response);
+            // Handle success (e.g., redirect to a success page)
+          },
+          (error) => {
+            console.error('Error registering user', error);
+            // Handle errors (e.g., display an error message)
+          }
+        );
+    }
+  
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe(
       response => {
@@ -20,6 +79,34 @@ export class UsersmanagementComponent implements OnInit {
         console.error(error);
       }
     );
+
+    
   }
+  get f() { return this.addUserForm.controls; }
+  onAdd() {
+    this.submitted = true;
+  
+    const user = this.addUserForm.value;
+    const apiUrl = `http://localhost:8020/api/auth/signup`; 
+  
+    if (this.addUserForm.invalid) {
+      return;
+    }
+  
+    this.http.post(apiUrl, user).subscribe(
+      (response) => {
+        // Handle success response
+        
+        console.log('API call successful', response);
+        // Handle success response here
+      },
+      (error) => {
+        // Handle error response
+        console.error('API call error', error);
+        // Handle error response here
+      }
+    );
+  }
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
@@ -15,6 +15,7 @@ import { ComponentsModule } from "./components/components.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 
 
@@ -39,6 +40,9 @@ import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
